ef9dedf970373542b8fd1d389051e1d599a26778
Mark Small <mark.small@astrosat.net>
2022-06-26 17:26:46 +0100

    feat(frontend): Add Storybook
    I've added storybook as a development tool, to create components
    in isolations, away from the app itself. This is a useful tool to
    highlight components we have, make then as independent/configurable
    as needed and showcase how they work, outside of the app itself.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'dark',
+    toolbar: {
+      icon: 'paintbrush',
+      items: [
+        { value: 'light', title: 'Light', left: '☀️' },
+        { value: 'dark', title: 'Dark', left: '🌒' },
+      ],
+    },
+  },
+};
