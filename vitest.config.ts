80ead2afb7a01b7dd7741b0161c6cead434ffb65
Mark Small <mark.small@astrosat.net>
2022-08-26 23:29:23 +0100

    fix(frontend): Fix path aliases
    While the path aliases worked, they still reported as errors in the
    VS Code IDE. I have now resolved this, so that it no longer does this.
    The cause was an incorrect configuration of the `baseUrl` in the
    `tsconfig.json` file. The rest of the changes are tidy ups.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 
-import path from 'path';
+import * as path from 'path';
 
 export default defineConfig({
   resolve: {
