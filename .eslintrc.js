2c616bf1904da0648ff9eaface6aeaed2ff5246b
Mark Small <mark.small@astrosat.net>
2022-08-28 23:51:12 +0100

    fix(frontend): Add eslint plugins to aid development
    Added testing and tailwindcss plugins to help development, these will
    suggest changes or where appropriate, go ahead and make the change
    suggested.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:storybook/recommended',
     'plugin:cypress/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/react',
+    'plugin:tailwindcss/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -27,11 +30,60 @@ module.exports = {
     'prettier',
     'import',
     'cypress',
+    'jest-dom',
+    'testing-library',
+    'tailwindcss',
   ],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/react-in-jsx-scope': 'off',
+    // 'import/order': [
+    //   'warn',
+    //   {
+    //     pathGroups: [
+    //       {
+    //         pattern: '~/**',
+    //         group: 'external',
+    //         position: 'after',
+    //       },
+    //     ],
+    //     'newlines-between': 'always-and-inside-groups',
+    //   },
+    // ],
+    // 'import/order': [
+    //   'warn',
+    //   {
+    //     groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+    //     pathGroups: [
+    //       {
+    //         pattern: 'react',
+    //         group: 'external',
+    //         position: 'before',
+    //       },
+    //       {
+    //         pattern: '@astrosat/**',
+    //         group: 'external',
+    //         position: 'before',
+    //       },
+    //     ],
+    //     pathGroupsExcludedImportTypes: ['builtin'],
+    //     'newlines-between': 'always',
+    //     alphabetize: {
+    //       order: 'asc',
+    //       caseInsensitive: true,
+    //     },
+    //   },
+    // ],
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
   },
   settings: {
     react: {
